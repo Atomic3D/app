29d7bba7280e09bd70ae50dd9ff0a426b8e5a580
Atomic3D <duke3datomic@mail.ru>
2023-02-24 23:11:36 +0700

    first commit

---
+++ components/MainContainer.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import A from "../components/A";
+const MainContainer = ({children, keywords}) => {
+    return (
+        <>
+            <Head>
+                <meta keywords={"qwe qwe" + keywords}></meta>
+                <title>Главная страница</title>
+            </Head>
+            <div className="navBar">
+                    <A href={'/'} text="Главная"/>
+                    <A href={'/users'} text="Пользователи"/>
+            </div>
+            <div>
+                {children}
+            </div>
+            <style jsx>
+                    {`
+                        .navBar {
+                            background: orange;
+                            padding: 15px;
+                        }
+                        
+                    `}                
+            </style>
+        </>
+    );
+};
+
+
+export default MainContainer;
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <h1>
+            Моя костомная страница с ошибкой
+        </h1>
+    )
+}
